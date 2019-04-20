278efbc49987fe3d4d70e85c75011720ca03827b
Jessica Pope <jessica.pope.co@gmail.com>
2019-04-20 18:09:15 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,8 +1,8 @@
 class Hash
   def keys_of(*values)
     keys = []
-    if Hash.has_value?(values)
-      keys<< hash.index(values)
+    if has_value?(values)
+      keys<< index(values)
     end
     return keys
   
